7132566b2813c9a9ed942dea6c7f01cb601dacba
Omar584 <112426827+Omar584@users.noreply.github.com>
2024-05-13 12:09:46 +0300

    edit

--- libraryEnv/iqraaWebsite/mainpages/urls.py
+++ libraryEnv/iqraaWebsite/mainpages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index , name='index') ,
+    path('books/',views.books,name='books'),
+    path('contact/',views.contact,name='contact'),
+    path('about/',views.about,name='about'),
 ]
--- libraryEnv/iqraaWebsite/mainpages/views.py
+++ libraryEnv/iqraaWebsite/mainpages/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'pages/main/index.html')
+
+def books(request):
+    return render(request,'pages/main/bookList.html')
+
+def contact(request):
+    return render(request,'pages/main/contact.html')
+
+def about(request):
+    return render(request,'pages/main/about.html')
+
+
