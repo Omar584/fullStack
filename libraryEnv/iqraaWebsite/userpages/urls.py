b5e4bdb6a59676f0ac1bf6d14c59cb29f3fec8bc
Omar584 <112426827+Omar584@users.noreply.github.com>
2024-05-13 14:30:41 +0300

    edit

--- libraryEnv/iqraaWebsite/userpages/urls.py
+++ libraryEnv/iqraaWebsite/userpages/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.userPage,),
+    path('',views.userPage,name='userPage'),
+    path('books/',views.userBooks , name = 'books'),
+    path('contact/' , views.userContact , name = 'contact') ,
+    path('about/' , views.userAbout , name = 'about') ,
+    path('profile/' , views.userProfile , name = 'profile') ,
+    path('changePassword/' , views.userChangePassword , name = 'changePassword')
 ]
 
--- libraryEnv/iqraaWebsite/userpages/views.py
+++ libraryEnv/iqraaWebsite/userpages/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 
 def userPage(request):
     return render(request,'pages/user/userPage.html')
+
+def userBooks(request):
+    return render(request,'pages/user/bookList.html')
+
+def userContact(request):
+    return render(request,'pages/user/contact.html')
+
+def userAbout(request):
+    return render(request,'pages/user/about.html')
+
+def userProfile(request):
+    return render(request,'pages/user/profile.html')
+
+def borrowedBooks(request):
+    return render(request,'pages/user/borrowedBooks.html')
+
+def userChangePassword(request):
+    return render(request,'pages/user/changePassword.html')
