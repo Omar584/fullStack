442be85c75dd2b4720f945909313f71350a176aa
Alaa saeed <113705228+Alaa188@users.noreply.github.com>
2024-05-13 23:16:22 +0300

    user

--- libraryEnv/iqraaWebsite/iqraaWebsite/settings.py
+++ libraryEnv/iqraaWebsite/iqraaWebsite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'book.apps.BookConfig',
     'adminpages.apps.AdminpagesConfig',
+    'userpages.apps.UserpagesConfig',
     'mainpages.apps.MainpagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
