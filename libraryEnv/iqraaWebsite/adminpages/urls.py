de32e6e53ce121ad64870ad758f31b624db1accf
Omar584 <112426827+Omar584@users.noreply.github.com>
2024-05-13 18:33:58 +0300

    admin pages are set

--- libraryEnv/iqraaWebsite/adminpages/urls.py
+++ libraryEnv/iqraaWebsite/adminpages/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.adminHome , name = 'adminHome'),
-    path('adminbooks/',views.adminbooks , name = 'adminbooks') ,
-    path('adminprofile',views.adminprofile , name = 'adminprofile')
-
+    path('adminbooks/',views.adminBooks , name = 'adminBooks') ,
+    path('adminprofile/',views.adminProfile , name = 'adminProfile') ,
+    path('addbook/' ,views.addBookPage , name ='addBookPage') ,
+    path('deletebook/' , views.deleteBookPage , name = 'deleteBookPage'),
+    path('editbook/',views.editBookPage , name = 'editBookPage' ) ,
+    path('changepassword/' , views.adminChangePassword , name = 'adminChangePassword') ,
 ]
--- libraryEnv/iqraaWebsite/adminpages/views.py
+++ libraryEnv/iqraaWebsite/adminpages/views.py
@@ -3,11 +3,23 @@ from django.shortcuts import render
 def adminHome(request):
     return render(request , 'pages/admin/adminPage.html')
 
-def adminbooks(request):
+def adminBooks(request):
     return render(request , 'pages/admin/bookList.html')
 
-def adminprofile(request):
+def adminProfile(request):
     return render(request , 'pages/admin/profile.html')
+
+def addBookPage(request):
+    return render(request,'pages/admin/addBook.html')
+
+def deleteBookPage(request):
+    return render(request, 'pages/admin/deleteBook.html')
+
+def editBookPage(request):
+    return render(request , 'pages/admin/editBook.html') ,
+
+def adminChangePassword(request):
+    return render(request , 'pages/admin/changePassword.html')
 
 
 
