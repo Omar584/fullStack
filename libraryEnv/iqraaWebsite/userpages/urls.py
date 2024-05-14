2371eb8249eceac1d6763136a142f54ca39327c6
Omar584 <112426827+Omar584@users.noreply.github.com>
2024-05-14 13:50:24 +0300

    edits

--- libraryEnv/iqraaWebsite/userpages/urls.py
+++ libraryEnv/iqraaWebsite/userpages/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    
+    path('' , views.userPage , name = 'userPage'),
+    path('bookList/' , views.userBooks , name = 'userBooks'),
+    path('borrowedBooks/' , views.borrowedbooks , name = 'borrowedBooks'),
+    path('changePassword/' , views.changePassword , name = 'changePassword'),
+    path('profile/' , views.profile , name = 'profile'),
+    path('contact' , views.contact , name = 'contact'),
 ]
--- libraryEnv/iqraaWebsite/userpages/views.py
+++ libraryEnv/iqraaWebsite/userpages/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from book.models import Book
 
-# Create your views here.
+def userPage(request):
+    return render(request , 'pages/user/userPage.html')
+
+def userBooks(request):
+    inventoryBooks = Book.objects.all()
+    return render(request , 'pages/user/bookList.html' , {'books' : inventoryBooks})
+
+def about(request):
+    return render(request , 'pages/user/about.html')
+
+def contact(request):
+    return render(request , 'pages/user/contact.html')
+
+def profile(request):
+    return render(request , 'pages/user/profile.html')
+
+def changePassword(request):
+    return render(request , 'pages/user/contact.html')
+
+def borrowedbooks(request):
+    return render(request , 'pages/user/borrowedBooks.html')
