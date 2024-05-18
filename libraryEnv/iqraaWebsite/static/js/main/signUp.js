058274dc1e8d64fd1c6c1d60a358098b70ad887c
Omar584 <112426827+Omar584@users.noreply.github.com>
2024-05-18 12:49:36 +0300

    edit

--- libraryEnv/iqraaWebsite/static/js/main/signUp.js
+++ libraryEnv/iqraaWebsite/static/js/main/signUp.js
@@ -1,41 +1,39 @@
 
 
-
 function validation() {
-    
-    var isValid=true;
-    
-    
-
-    var username = document.getElementById('uname').value;
-    var password = document.getElementById('pass').value;
-    var confirmPassword = document.getElementById('cpass').value;
-    var email = document.getElementById('em').value;
-    var isAdmin = document.getElementById('is_admin').checked;
-
-    if(username.length < 5) {
-        document.getElementById('username').innerHTML = 'sjjername must be at leauhqej,krhcj7.';
-        isValid = false;
-    }
-    
-    if(password.length < 8 ) {
-        document.getElementById('password').innerHTML = 'Password must be at least 8 characters long' ;
-        isValid = false;
-    }
-    if( !/[!#$%*@]/.test(password)) {
-        document.getElementById('passtest').innerHTML = 'please enter one of the following special characters: !, #, $, %, or *' ;
-        isValid = false;
-    }
-
-    if(password !== confirmPassword) {
-        document.getElementById('confirmpassword').innerHTML = 'Passwords do not match. Please try again.' ;
-        isValid = false;
-    }
-    // If validations pass, continue with form submissi
-    // window.location.href = "{% url 'index' %}";
-    return isValid;
-}
         
-    
+        var username = document.getElementById('uname').value;
+        var password = document.getElementById('pass').value;
+        var confirmPassword = document.getElementById('cpass').value;
+        var email = document.getElementById('em').value;
+        var isAdmin = document.getElementById('is_admin').checked;
 
-    document.querySelector('form').addEventListener('submit', validation);
+        if(username.length < 5) {
+            document.getElementById('username').innerHTML = 'Username must be at least 5 characters long.' ;
+            event.preventDefault(event) ;
+        }
+
+        if(username.includes(' ')) {
+            document.getElementById('username').innerHTML = 'Username cannot contain spaces.' ;
+            return false ;
+        }
+        if(password.length < 8 ) {
+            document.getElementById('password').innerHTML = 'Password must be at least 8 characters long' ;
+            return false ;
+        }
+
+        if( !/\d/.test(password) ) {
+            document.getElementById('password').innerHTML = 'password must contain at least one digit ' ;
+            return false ;
+        }
+
+        if( !/[!#$%*@]/.test(password)) {
+            document.getElementById('passtest').innerHTML = 'please enter one of the following special characters: !, #, $, %, or *' ;
+            return false;
+        }
+
+        if(password !== confirmPassword) {
+            document.getElementById('password').innerHTML = 'Passwords do not match. Please try again.' ;
+            return false ;
+        }
+    }
